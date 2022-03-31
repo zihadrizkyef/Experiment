07762e19b177a682f809a15d66398f03bcc9a669
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-03-31 15:51:37 +0700

    add view tree observer testing

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zref.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zref.experiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.text1.viewTreeObserver.addOnGlobalLayoutListener {
+            Log.i("AOEU", "text1 ${binding.text1.width} -> ${binding.text2.width}")
+        }
+
+        binding.text2.viewTreeObserver.addOnGlobalLayoutListener {
+            Log.i("AOEU", "text2 ${binding.text2.width} -> ${binding.text1.width}")
+        }
+
+        binding.root.viewTreeObserver.addOnGlobalLayoutListener {
+            Log.i("AOEU", "root ${binding.text1.width} -> ${binding.text2.width}")
+        }
     }
 }
