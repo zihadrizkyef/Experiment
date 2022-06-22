528797c90e0203ce82617f7a58f2e2913511c2aa
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-06-22 10:00:08 +0700

    scale bitmap

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zref.experiment
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zref.experiment.databinding.ActivityMainBinding
@@ -11,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.qrcode)
+        val scaled = Bitmap.createScaledBitmap(bitmap, 500, 500, true)
+        binding.imageView.setImageBitmap(bitmap)
+        binding.imageView2.setImageBitmap(scaled)
     }
 }
