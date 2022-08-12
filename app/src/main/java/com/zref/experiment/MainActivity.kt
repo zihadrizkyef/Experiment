1b0e2007b988cf1ed2729763654e507860ed3d9b
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-08-12 16:35:31 +0700

    GSON parsing string to double

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -2,6 +2,8 @@ package com.zref.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
 import com.zref.experiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val json = """{name:"zihad", length:"56.88"}"""
+        val mobile = Gson().fromJson(json, Mobile::class.java)
+        Log.i("AOEU", "mobile $mobile")
+        Log.i("AOEU", "length ${mobile.length - 32}")
     }
+
+    data class Mobile(val name: String, val length: Double)
 }
