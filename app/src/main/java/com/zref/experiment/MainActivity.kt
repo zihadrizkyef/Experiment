fb2b39486501c4035ceeb69ce1db707a1cd27a33
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-11-15 12:41:56 +0700

    add currency-extensio

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -3,6 +3,10 @@ package com.zref.experiment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zref.experiment.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +15,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val list = listOf(
+            123_456_789.0,
+            123_456_789.1,
+            123_456_789.01,
+            123_456_789.100,
+            123_456_789.1001,
+            123_456_789.1010,
+            123_456_789.1100,
+            123_456_789.1101,
+            123_456_789.1110,
+            123_456_789.1110000001,
+            123_456_789.111000000,
+            123_456_789.000000,
+        )
+
+        binding.text.text = list.joinToString("\n")
+        binding.text2.text = list.joinToString("") { "->       " + it.toCurrency("Rp ") + "\n" }
+    }
+
+    fun Double.toCurrency(currency: String = ""): String {
+        val symbolConfig = if (currency.uppercase() == "RP") {
+            val decFormat = DecimalFormatSymbols.getInstance()
+            decFormat.groupingSeparator = '.'
+            decFormat.decimalSeparator = ','
+            decFormat
+        } else {
+            DecimalFormatSymbols.getInstance(Locale.US)
+        }
+
+        symbolConfig.currencySymbol = ""
+
+
+        val formatter = NumberFormat.getInstance() as DecimalFormat
+        formatter.decimalFormatSymbols = symbolConfig
+        formatter.maximumFractionDigits = 340
+
+        return "$currency${formatter.format(this)}"
     }
 }
