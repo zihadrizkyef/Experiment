1a9185c4fa6215da704cdbdc3157013eca80104b
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-11-16 15:34:20 +0700

    add date-parsing

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -2,7 +2,11 @@ package com.zref.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.zref.experiment.databinding.ActivityMainBinding
+import org.joda.time.DateTime
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val tgl = "2022-11-15"
+        val parser = SimpleDateFormat("yyyy-MM-dd")
+        val date = parser.parse(tgl)
+
+
+        val date2 = Calendar.getInstance().let {
+            it.set(1970, 0, 0, 0, 0)
+            it.time
+        }
+        val calendar = Calendar.getInstance()
+        calendar.time = date2
+
+        val date3 = DateTime(1970, 1, 1, 0, 0, 0).toDate()
+
+        Log.i("AOEU", date3.toString())
     }
 }
