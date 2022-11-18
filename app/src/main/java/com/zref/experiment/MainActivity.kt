753f8a7ed82b3ad0a52e22a4e1a3680663598d3e
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-11-18 13:52:24 +0700

    add selector drawable

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -11,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonAnu.setOnClickListener {
+            binding.buttonAnu.isSelected = !binding.buttonAnu.isSelected
+        }
     }
 }
