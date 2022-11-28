393e2e81ca61028af98455ff9dab7705eab5c6d3
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2022-11-28 16:18:21 +0700

    add gson parsing from date 0000-00-00 00:00:00

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zref.experiment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.gson.Gson
 import com.zref.experiment.databinding.ActivityMainBinding
 import org.joda.time.DateTime
 import java.text.SimpleDateFormat
@@ -16,11 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val json = """{"name":"Zihad", "date":"0000-00-00 00:00:00"}"""
+        val gson = Gson()
+        val user = gson.fromJson(json, User::class.java)
+        Log.i("AOEU", "date : ${user.date}")
+
 
         val tgl = "2022-11-15"
         val parser = SimpleDateFormat("yyyy-MM-dd")
         val date = parser.parse(tgl)
-
 
         val date2 = Calendar.getInstance().let {
             it.set(1970, 0, 0, 0, 0)
---
+++ app/src/main/java/com/zref/experiment/User.kt
@@ -0,0 +1,6 @@
+package com.zref.experiment
+
+data class User(
+    val name: String,
+    val date: String,
+)
