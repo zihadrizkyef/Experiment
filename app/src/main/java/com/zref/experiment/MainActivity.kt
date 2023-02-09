6bc2c0163edb6e0eab2854499445809b47edb58b
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2023-02-09 13:03:29 +0700

    Timer task testing

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -2,7 +2,10 @@ package com.zref.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import com.zref.experiment.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,5 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var timer = Timer()
+        var taskTimer: TimerTask = object : TimerTask() {
+            override fun run() {
+                Log.i("AOEU", "timer executed")
+            }
+        }
+        timer.scheduleAtFixedRate(taskTimer, 0, 2000L)
+
+        Handler().postDelayed({
+            timer.cancel()
+            timer = Timer()
+            taskTimer = object : TimerTask() {
+                override fun run() {
+                    Log.i("AOEU", "timer executed changed")
+                }
+            }
+            timer.scheduleAtFixedRate(taskTimer, 0, 1000)
+        }, 5000)
     }
 }
