df8434d1ba53d7505978183809f1238c5f2301d8
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2023-03-15 10:55:06 +0700

    countdown-timer

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zref.experiment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.zref.experiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val countDownTimer = object : CountDownTimer((1000*60*60*3)+(1000*90), 1000L) {
+            override fun onTick(millisUntilFinished: Long) {
+                val seconds = (millisUntilFinished / 1000) % 60
+                val minutes = (millisUntilFinished / (1000 * 60) % 60)
+                val hours = (millisUntilFinished / (1000 * 60 * 60) % 24)
+                binding.textView.text = "($hours:$minutes:$seconds)"
+            }
+
+            override fun onFinish() {
+                binding.textView.text = "DONE"
+            }
+        }.start()
+
+        binding.textView.setOnClickListener {
+            countDownTimer.cancel()
+        }
     }
 }
