c203f4b5a7a3835700a3715df9d533770787dcb2
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2023-05-29 09:23:09 +0700

    animasi up down

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -1,15 +1,51 @@
 package com.zref.experiment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.zref.experiment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var isUp = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonMerah.setOnClickListener {
+            binding.textHelloAndre.isVisible = true
+            isUp = !isUp
+            if (isUp) {
+                animUp()
+            } else {
+                animDown()
+            }
+        }
     }
+
+    fun animUp() {
+        binding.textHelloWorld.animate()
+            .translationY(-300.dp)
+            .alpha(0F)
+            .start()
+        binding.textHelloAndre.animate()
+            .translationY(0F)
+            .alpha(1F)
+            .start()
+    }
+
+    fun animDown() {
+        binding.textHelloWorld.animate()
+            .translationY(0F)
+            .alpha(1F)
+            .start()
+        binding.textHelloAndre.animate()
+            .translationY(300.dp)
+            .alpha(0F)
+            .start()
+    }
+
+    val Int.dp: Float get() = (this / resources.displayMetrics.density)
 }
