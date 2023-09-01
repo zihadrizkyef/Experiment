9960babed7d783ec121e15e39977868571738994
Zihad Rizky Edwin Fikri <zihadrizkyef@gmail.com>
2023-09-01 13:51:41 +0700

    mutex

--- app/src/main/java/com/zref/experiment/MainActivity.kt
+++ app/src/main/java/com/zref/experiment/MainActivity.kt
@@ -1,15 +1,63 @@
 package com.zref.experiment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.zref.experiment.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel = ViewModelAnu()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel.a.observe(this) {
+            binding.textView.text = binding.textView.text.toString() + "\n" + it.toString()
+        }
+
+        viewModel.add1()
+        viewModel.add2()
+    }
+
+    class ViewModelAnu : ViewModel() {
+        val a = MutableLiveData<Int>()
+        val mutex = Mutex()
+
+        fun add1() {
+            viewModelScope.launch {
+                flow<Any> {
+                    Thread.sleep(3000)
+                    a.postValue(3000)
+                }.flowOn(Dispatchers.IO).collect()
+            }
+        }
+
+        fun add2() {
+            viewModelScope.launch {
+                mutex.withLock {
+                    withContext(Dispatchers.IO) {
+                        a.postValue(add2b())
+                    }
+                }
+            }
+        }
+
+        private fun add2b(): Int {
+            Thread.sleep(1000)
+            return 1000
+        }
     }
 }
